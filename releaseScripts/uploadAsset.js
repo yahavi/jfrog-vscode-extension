5b7bd1eec7bc55c88f358a93f0b8c11689e5ed40
yahavi <yahavi@jfrog.com>
2019-10-03 11:09:48 +0300

    Add release script

---
+++ releaseScripts/uploadAsset.js
@@ -0,0 +1,19 @@
+const github = require('@actions/github');
+const core = require('@actions/core')
+const fs = require('fs');
+
+const githubToken = core.getInput('GITHUB_TOKEN');
+const octokit = new github.GitHub(githubToken);
+
+let vsixFileName = 'jfrog-vscode-extension-' + process.argv[j] + '.vsix';
+let vsixFilePath = '../' + vsixFileName;
+
+octokit.repos.uploadReleaseAsset({
+    file: fs.createReadStream(vsixFilePath),
+    headers: {
+        'content-length': fs.statSync(vsixFilePath).size,
+        'content-type': 'application/zip'
+    },
+    name: vsixFileName,
+    url: 'https://uploads.github.com/repos/yahavi/jfrog-vscode-extension/releases/' + process.argv[j] + '/assets{?name,label}'
+});
