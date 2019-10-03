fbbe0eba2beb82de0f72c34bcdf7537059d0e39e
yahavi <yahavi@jfrog.com>
2019-10-03 12:10:32 +0300

    ..

--- releaseScripts/uploadAsset.js
+++ releaseScripts/uploadAsset.js
@@ -2,21 +2,25 @@ const github = require('@actions/github');
 const core = require('@actions/core');
 const fs = require('fs');
 
-const githubToken = core.getInput('GITHUB_TOKEN');
+const githubToken = core.getInput('GITHUB_TOKEN', { required: true });
 const octokit = new github.GitHub(githubToken);
 
-core.debug('Releasing ' + process.env.GITHUB_REF);
 let tag = process.env.GITHUB_REF.split('/')[2];
+core.debug('Releasing ' + tag);
 
 let vsixFileName = 'jfrog-vscode-extension-' + tag + '.vsix';
 let vsixFilePath = '../' + vsixFileName;
 
-octokit.repos.uploadReleaseAsset({
-    file: fs.createReadStream(vsixFilePath),
-    headers: {
-        'content-length': fs.statSync(vsixFilePath).size,
-        'content-type': 'application/zip'
-    },
-    name: vsixFileName,
-    url: 'https://uploads.github.com/repos/yahavi/jfrog-vscode-extension/releases/' + tag + '/assets{?name,label}'
-});
+octokit.repos
+    .uploadReleaseAsset({
+        file: fs.createReadStream(vsixFilePath),
+        headers: {
+            'content-length': fs.statSync(vsixFilePath).size,
+            'content-type': 'application/zip'
+        },
+        name: vsixFileName,
+        url: 'https://uploads.github.com/repos/yahavi/jfrog-vscode-extension/releases/' + tag + '/assets{?name,label}'
+    })
+    .catch(error => {
+        core.error(error);
+    });
